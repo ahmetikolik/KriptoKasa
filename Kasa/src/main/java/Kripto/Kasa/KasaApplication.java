a7acfd9e81a5c7ecf86211461d8b6d2629d844ae
ahmetikolik <ahmet.yildirim5@std.yildiz.edu.tr>
2026-07-13 22:02:10 +0300

    Initial CryptoPal project setup

---
+++ Kasa/src/main/java/Kripto/Kasa/KasaApplication.java
@@ -0,0 +1,13 @@
+package Kripto.Kasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasaApplication.class, args);
+	}
+
+}
---
+++ Kasa/src/test/java/Kripto/Kasa/KasaApplicationTests.java
@@ -0,0 +1,13 @@
+package Kripto.Kasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
